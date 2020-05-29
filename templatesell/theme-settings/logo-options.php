130e0d61d7180a0e9c41c7992b43dd9f5e21a994
kafleg <merohappylife@gmail.com>
2020-05-29 18:47:52 +0545

    text domain fixed

--- templatesell/theme-settings/logo-options.php
+++ templatesell/theme-settings/logo-options.php
@@ -7,8 +7,8 @@ $wp_customize->add_setting( 'polite_options[polite_logo_width_option]', array(
     'sanitize_callback' => 'absint'
 ) );
 $wp_customize->add_control( 'polite_options[polite_logo_width_option]', array(
-   'label'     => __( 'Logo Width', 'springy' ),
-   'description' => __('Adjust the logo width. Minimum is 100px and maximum is 600px.', 'springy'),
+   'label'     => __( 'Logo Width', 'polite' ),
+   'description' => __('Adjust the logo width. Minimum is 100px and maximum is 600px.', 'polite'),
    'section'   => 'title_tagline',
    'settings'  => 'polite_options[polite_logo_width_option]',
    'type'      => 'range',
