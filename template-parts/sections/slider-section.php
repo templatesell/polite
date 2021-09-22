5e94b32c76fcd64d82e2308ad56ef4fb3ea6683e
Sagar Tamang <mi5t4n@gmail.com>
2021-09-22 22:14:35 +0545

    Fix - Undefined  variable.

--- template-parts/sections/slider-section.php
+++ template-parts/sections/slider-section.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
  * Polite Slider Function
  * @since Polite 1.0.0
@@ -24,8 +24,8 @@ $slide_id = absint($polite_theme_options['polite-select-category']);
 		$slider_query = new WP_Query($args);
 		if ($slider_query->have_posts()): ?>
     <div class="container">
-    <div class="modern-slider" data-slick='<?php echo $slick_args_encoded; ?>'>
-				<?php while ($slider_query->have_posts()) : $slider_query->the_post(); 
+    <div class="modern-slider" data-slick='<?php echo isset( $slick_args_encoded ) ? $slick_args_encoded : ''; ?>'>
+				<?php while ($slider_query->have_posts()) : $slider_query->the_post();
           if(has_post_thumbnail()){
           $image_id = get_post_thumbnail_id();
           $image_url = wp_get_attachment_image_src( $image_id,'',true );
@@ -41,7 +41,7 @@ $slide_id = absint($polite_theme_options['polite-select-category']);
                        $categories = get_the_category();
                        if ( ! empty( $categories ) ) {
                           echo '<a class="s-cat" href="'.esc_url( get_category_link( $categories[0]->term_id ) ).'">'.esc_html( $categories[0]->name ).'</a>';
-                      }                                 
+                      }
                     ?>
             		    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
                     <div class="entry-meta">
@@ -53,7 +53,7 @@ $slide_id = absint($polite_theme_options['polite-select-category']);
                     </div>
             	</div>
             </div>
-            
+
           </div>
         </div>
         <?php } endwhile;
